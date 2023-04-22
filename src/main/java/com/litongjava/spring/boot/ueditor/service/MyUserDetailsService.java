bf87dd3a9c2f918e8f9a6284b7f2c68a6e5c433c
litongjava <litongjava@qq.com>
2023-04-22 13:41:22 +0800

    将用户名和密码提取到配置文件中

--- src/main/java/com/litongjava/spring/boot/ueditor/service/MyUserDetailsService.java
+++ src/main/java/com/litongjava/spring/boot/ueditor/service/MyUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.litongjava.spring.boot.ueditor.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,16 +19,21 @@ public class MyUserDetailsService implements UserDetailsService {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  @Value("${project.user.name}")
+  private String dbUsername;
+  @Value("${project.user.password}")
+  public String dbPassword;
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    log.info("project.name:{},project.password:{}", dbUsername,dbPassword);
     log.info("用户的用户名: {}", username);
     // TODO 根据用户名，查找到对应的密码，与权限
 
-    String password = passwordEncoder.encode("robot_123456#");
+    String password = passwordEncoder.encode(dbPassword);
     log.info("password: {}", password);
 
     // 封装用户信息，并返回。参数分别是：用户名，密码，用户权限
-    User user = new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    User user = new User(dbUsername, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     return user;
   }
 }
